ec8a59ee481b736e4224a51c87af93472fa4949b
Jonathan Burman <jonathan.burman@timeout.com>
2020-10-16 12:50:13 +0100

    update ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -l
 
 set -xe
-
+sh -c "mkdir -p ~/.ssh"
+sh -c "echo ${ssh-key} > ~/.ssh/id_rsa"
+sh -c "chmod 600 ~/.ssh/id_rsa"
+sh -c "ssh-keyscan github.com >> ~/.ssh/known_hosts"
 sh -c "aws configure set region ${aws_region}"
 sh -c "aws configure set aws_access_key_id ${aws_access_key_id}"
 sh -c "aws configure set aws_secret_access_key ${aws_secret_access_key}"
