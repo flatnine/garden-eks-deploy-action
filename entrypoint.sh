2915b9540572159ace8b162cc743888ce5b4cb52
Jonathan Burman <jonathan.burman@timeout.com>
2020-10-16 15:49:31 +0100

    ssh-key

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -l
 
 set -xe
-sh -c "mkdir -p ~/.ssh"
-echo ${ssh_key} > ~/.ssh/id_rsa
-sh -c "chmod 600 ~/.ssh/id_rsa"
-sh -c "ssh-keyscan github.com >> ~/.ssh/known_hosts"
+sh -c "mkdir -p /root/.ssh"
+echo ${ssh_key} > /root/.ssh/id_rsa
+sh -c "chmod 600 /root/.ssh/id_rsa"
+sh -c "ssh-keyscan github.com >> /root/.ssh/known_hosts"
+sh -c "chmod 700 /root/.ssh/"
+sh -c "chmod 755 /root/.ssh/known_hosts"
 sh -c "aws configure set region ${aws_region}"
 sh -c "aws configure set aws_access_key_id ${aws_access_key_id}"
 sh -c "aws configure set aws_secret_access_key ${aws_secret_access_key}"
